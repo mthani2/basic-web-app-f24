2b6c8068302f070c3ba121ca3c824be71ceb864e
Moza Al Thani <mthani2@andrew.cmu.edu>
2024-10-24 16:41:47 +0300

    added question 2

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -13,8 +13,15 @@ export default function QueryProcessor(query: string): string {
     return "mthani2";
   }
 
-  if (query.toLowerCase().includes("what is your name")) {
+  if (query.includes("What is your name")) {
     return "mthani2-313";
+  }
+
+  if (query.toLowerCase().includes("which of the following numbers is the largest")) {
+    // Extract numbers from the query
+    const numbers = query.match(/\d+/g).map(Number);
+    // Find and return the largest number
+    return Math.max(...numbers).toString();
   }
 
   return "";
